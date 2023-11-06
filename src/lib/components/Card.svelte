9524eae339a6772d2f03773762fda223a2621f26
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-06 15:46:31 -0700

    fixed size and padding of the article element; changed the text-color of the cards

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -12,14 +12,16 @@
 
 <!--Card article-->
 
-  <article class="lg:bg-pink-400 text-xl p-8 m-auto text-center  lg:border-x-[600px] lg:border-y-[50px] border-solid">
+  <article class="w-[550px] lg:bg-red-50 bg-contain  bg-cover text-xl text-center border-solid scale-75 m-auto rounded-[10%] p-8 hover:bg-orange-200">
+   <div>
     <a href={url}>
-      <img src={img} class="w-[250px] h-[250px] m-auto" alt="player profile pic">
+      <img src={img} class="w-[250px] h-[250px] m-auto rounded-[5%]" alt="player profile pic">
       <h3 class="p-4 text-3xl">{name}</h3>
-      <p class="p-1 text-green-200">{age}</p>
-      <p class="p-1 text-green-200">{location}</p>
-      <p class="p-1 text-green-200">{position}</p>
-      <p class="p-1 text-green-200">{description}</p>
-      <p class="p-1 text-green-200">{team}</p>
+      <p class="p-1 text-orange-950">{age}</p>
+      <p class="p-1 text-orange-950">{location}</p>
+      <p class="p-1 text-orange-950">{position}</p>
+      <p class="p-1 text-orange-950">{description}</p>
+      <p class="p-1 text-orange-950">{team}</p>
     </a>
+  </div> 
   </article>
