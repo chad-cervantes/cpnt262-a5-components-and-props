d040de3ed05026627c11bce41bff50a76ad8debe
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-06 09:09:32 -0700

    added img element in the script and article elements

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -5,12 +5,14 @@
   export let location;
   export let position;
   export let team;
+  export let img;
 </script>
 
 <!--Card article-->
 <article>
   <div></div>
   <h3>{name}</h3>
+  <img src={img} alt="">
   <p>{age}</p>
   <p>{location}</p>
   <p>{position}</p>
