3d94a8fbea4cb5e53598c7866ed5b07a892bf958
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-06 08:40:47 -0700

    made 2 extra card objects and rendered their info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,31 @@
   import Card from '../lib/components/Card.svelte';
   
   //Card object array
-  const cardObj = {
-    name: 'Jose',
+  const cardObj = [
+    {name: 'Jose',
     age:'25 years old',
     location: 'Los Angeles',
     position: 'striker',
-    team: 'LA Galaxy'
-  };
+    team: 'LA Galaxy'},
+
+    {name: 'Daniel',
+    age:'23 years old',
+    location: 'New York City',
+    position: 'defender',
+    team: 'NYCFC'},
+
+    {name: 'Reggie',
+    age:'21 years old',
+    location: 'Seattle',
+    position: 'midfielder',
+    team: 'Seattle Sounders'}
+  ];
  
 </script>
 
-<!--Rendered card components-->
-<Card {...cardObj} />
+<!--For each loop-->
+{#each cardObj as card}
+  <Card {...card} />
+{/each}
+
 
