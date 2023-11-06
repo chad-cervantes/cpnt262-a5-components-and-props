3e65dc72669988651ebb5e61fc1b50435a5a36f8
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-06 09:51:04 -0700

    made Header.svelte component in lib folder

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+   const navItems = [
+    {name: 'Home', href: '/'},
+    {name: 'About', href: '/about'},
+    {name: 'Story', href: '/story'},
+    {name: 'Contact', href: '/contact'}
+  ];
+
+</script>
+
+
+<header>
+  <div>logo</div>
+  <nav>
+    <ul class>
+      {#each navItems as {name, href}}
+        <li><a {href}>{name}</a></li>
+      {/each}  
+    </ul>
+  </nav>
+</header>
