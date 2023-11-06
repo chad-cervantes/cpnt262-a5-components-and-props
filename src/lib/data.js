d90c4cfc9109a24e09f3556072e25942da01c3f5
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-06 14:25:45 -0700

    added cardObj to data.js from +page.svelte

---
+++ src/lib/data.js
@@ -0,0 +1,28 @@
+//Card object array
+export const cardObj = [
+  {img: '/images/profile-pic.jpg',
+  name: 'Jose',  
+  age:'25 years old',
+  location: 'Los Angeles',
+  description: 'height of 6ft',
+  position: 'striker',
+  team: 'LA Galaxy'},
+
+  { img: '/images/profile-pic.jpg',
+  name: 'Daniel',
+  age:'23 years old',
+  location: 'New York City',
+  description: 'height of 5.8ft',
+  position: 'defender',
+  team: 'NYCFC'},
+
+  {img: '/images/profile-pic.jpg',
+  name: 'Reggie',
+  age:'21 years old',
+  location: 'Seattle',
+  description: 'height of 5.6ft',
+  position: 'midfielder',
+  team: 'Seattle Sounders'}
+];
+  
+
