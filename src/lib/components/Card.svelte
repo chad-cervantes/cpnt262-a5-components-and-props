975bc74d6fa922ce6563f8c9b66e3cb84067c5d9
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-06 14:26:49 -0700

    re-added anchor tag to the card since it looked weird

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -12,7 +12,7 @@
 <!--Card article-->
 
   <article class="lg:bg-pink-400 text-xl p-8 m-auto text-center  lg:border-x-[600px] lg:border-y-[50px] border-solid">
-    <a href="#" > 
+    <a href='#'> 
       <img src={img} class="w-[250px] h-[250px] m-auto" alt="player profile pic">
       <h3 class="p-4 text-3xl">{name}</h3>
       <p class="p-1 text-green-200">{age}</p>
