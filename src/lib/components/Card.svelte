e7cd111851aef096ff0a5c23896244c115c72e42
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-06 13:40:50 -0700

    added an anchor tag for the url for the card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -9,11 +9,14 @@
 </script>
 
 <!--Card article-->
-<article class="lg:bg-pink-400 text-xl p-8 m-auto text-center  lg:border-x-[600px] lg:border-y-[50px] border-solid">
-  <img src={img} class="w-[250px] h-[250px] m-auto" alt="player profile pic">
-  <h3 class="p-4 text-3xl">{name}</h3>
-  <p class="p-1 text-green-200">{age}</p>
-  <p class="p-1 text-green-200">{location}</p>
-  <p class="p-1 text-green-200">{position}</p>
-  <p class="p-1 text-green-200">{team}</p>
-</article>
+
+  <article class="lg:bg-pink-400 text-xl p-8 m-auto text-center  lg:border-x-[600px] lg:border-y-[50px] border-solid">
+    <a href="#" > 
+      <img src={img} class="w-[250px] h-[250px] m-auto" alt="player profile pic">
+      <h3 class="p-4 text-3xl">{name}</h3>
+      <p class="p-1 text-green-200">{age}</p>
+      <p class="p-1 text-green-200">{location}</p>
+      <p class="p-1 text-green-200">{position}</p>
+      <p class="p-1 text-green-200">{team}</p>
+    </a>
+  </article>
