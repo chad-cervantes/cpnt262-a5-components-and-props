ca259a1c288f81533544b89aa1b1529a091f969c
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-07 15:26:21 -0700

    made cards mobile-friendly from grid-cols-2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
+<!--Canada Soccer League title-->
 <h1 class="p-6 text-center lg:text-7xl bg-rose-400 font-mono"> Canada Soccer League</h1>
 
+<!--Hero Section image-->
 <div>
   <img src='/images/pexels-johannes-plenio-1103969.jpg' alt="soccer stadium" class="w-[1590px]">
 </div>
 
-<h1 class="p-6 text-center lg:text-7xl bg-rose-400 font-mono">List of current CSL soccer players</h1>
+<!--List of players title-->
+<h2 class="p-6 text-center lg:text-7xl bg-rose-400 font-mono">List of current CSL soccer players</h2>
 
 <script>
   //imported card info
@@ -15,7 +18,7 @@
 </script>
 
 <!--For each loop-->
-<section class="grid grid-cols-2">
+<section class="lg:grid grid-cols-2">
   {#each cardObj as card}
     <Card {...card} />
   {/each}
