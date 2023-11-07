d7ac222007e40ff167d8d98839510ea81cbd83f3
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-07 13:20:04 -0700

    made article element of cards mobile friendly

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -12,7 +12,7 @@
 
 <!--Card article-->
 
-  <article class="w-[550px] lg:bg-red-300 bg-contain  bg-cover text-xl text-center border-solid scale-75 m-auto rounded-[5%] p-8 hover:bg-orange-400 active:bg-teal-500 transition ease-linear duration-150">
+  <article class="lg:w-[550px] bg-red-300 bg-contain  bg-cover text-xl text-center border-solid scale-75 m-auto rounded-[5%] p-8 hover:bg-orange-400 active:bg-teal-500 transition ease-linear duration-150">
    <div>
     <a href={url}>
       <img src={img} class="w-[500px] h-[350px] m-auto rounded-[5%]" alt="player profile pic">
