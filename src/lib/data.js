99c1f54d785f9765d6aef077a54ec0e70036fbe4
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-07 16:13:05 -0700

    made comments to identify which card information belonged to who

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,6 @@
 //Card object array
 export const cardObj = [
+  //Jose's card info
  {img: '/images/pexels-pixabay-jose-card.jpg',
   title: 'Jose',  
   age:'25 years old',
@@ -9,6 +10,7 @@ export const cardObj = [
   team: 'Calgary Sheriffs',
   url: 'https://www.mlssoccer.com/'},
 
+  //Daniel's card info
  {img: '/images/pexels-mica-asato-daniel-card.jpg',
   title: 'Daniel',
   age:'23 years old',
@@ -18,6 +20,7 @@ export const cardObj = [
   team: 'Vancouver Killerwhales',
   url:'https://www.mlssoccer.com/'},
 
+  //Reggie's card info
  {img: '/images/pexels-evani-эвани-reggies-card.jpg',
   title: 'Reggie',
   age:'21 years old',
@@ -27,6 +30,7 @@ export const cardObj = [
   team: 'Winnipeg Bisons',
   url: 'https://www.mlssoccer.com/'},
 
+  //Yamal's card info
  {img: '/images/pexels-pixabay-yamals-card.jpg',
   title: 'Yamal',
   age:'20 years old',
